9d5bd08b4e37e13768f0922040e1ac3bbe5c4bdf
DESKTOP-C5MKHEH\msh14 <msh1411@naver.com>
2023-11-27 09:06:38 +0900

    민상훈/1127-01/상품등록

--- frontRepo/admin/js/msh/enrollProduct.js
+++ frontRepo/admin/js/msh/enrollProduct.js
@@ -1,22 +1,16 @@
-// $("#file").on('change',function(){
-//     var fileName = $("#file").val();
-//     $(".upload-name").val(fileName);
-//   });
+function previewImage(event) {
+    const fileInput = event.target;
+    const imagePreview = document.getElementById('image-preview');
 
-        // 파일 선택 시 미리보기 함수
-        function previewImage(event) {
-            const fileInput = event.target;
-            const imagePreview = document.getElementById('image-preview');
+    const file = fileInput.files[0];
 
-            const file = fileInput.files[0];
+    if (file) {
+        const reader = new FileReader();
 
-            if (file) {
-                const reader = new FileReader();
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+        };
 
-                reader.onload = function(e) {
-                    imagePreview.src = e.target.result;
-                };
-
-                reader.readAsDataURL(file);
-            }
-        }
+        reader.readAsDataURL(file);
+    }
+}
