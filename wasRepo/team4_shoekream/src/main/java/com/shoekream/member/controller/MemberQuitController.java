fd61e644c581da54d97d653fae40682d14e6d2a2
orlando <glee1470@naver.com>
2023-12-03 16:08:51 +0900

    이경민/1203/회원탈퇴 작업중

---
+++ wasRepo/team4_shoekream/src/main/java/com/shoekream/member/controller/MemberQuitController.java
@@ -0,0 +1,53 @@
+package com.shoekream.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shoekream.member.MemberVo;
+import com.shoekream.member.service.MemberService;
+
+@WebServlet("/member/quit")
+
+public class MemberQuitController extends HttpServlet{
+	
+	// 탈퇴 화면 보여주기
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/WEB-INF/views/member/quit.jsp").forward(req, resp);
+		
+	}
+	
+	// 회원 탈퇴 처리
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			// 데이터
+			MemberVo loginMember = (MemberVo) req.getSession().getAttribute("loginMember");
+			
+			// service
+			MemberService ms = new MemberService();
+			int result = ms.quit(loginMember);
+			
+			// result == view
+			if(result != 1) {
+				throw new Exception("탈퇴할 수 없는 회원");
+			}
+			
+			// 로그아웃
+			req.getSession().invalidate();
+			// 홈페이지로 redirect
+			resp.sendRedirect("/shoekream/home");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			resp.sendRedirect("/shoekream/member/quit");
+		}
+		
+	}
+}
