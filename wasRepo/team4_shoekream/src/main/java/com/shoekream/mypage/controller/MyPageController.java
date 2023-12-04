76465adf13b2c8c8d36a9215dd983eb37a458708
orlando <glee1470@naver.com>
2023-12-05 08:56:13 +0900

    이경민/1205/마이페이지 작업 시작

--- wasRepo/team4_shoekream/src/main/java/com/shoekream/mypage/controller/MyPageController.java
+++ wasRepo/team4_shoekream/src/main/java/com/shoekream/mypage/controller/MyPageController.java
@@ -8,12 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.shoekream.member.MemberVo;
+import com.shoekream.member.service.MemberService;
+
 @WebServlet("/mypage/main")
 
 public class MyPageController extends HttpServlet{
 	
+	// 마이페이지 메인 화면 띄우기
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// data
+		MemberVo loginMember = (MemberVo) req.getSession().getAttribute("loginMember");
+		
+		if(loginMember == null) {
+			resp.sendRedirect("/shoekream/member/login");
+		}
+		
+		
 		
 		req.getRequestDispatcher("/WEB-INF/views/mypage/my.jsp").forward(req, resp);
 		
