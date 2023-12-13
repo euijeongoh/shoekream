4c1e031b2898f4aead55a4032bb4ed37886bf97b
oldstudent4312 <x930406@gmail.com>
2023-12-13 17:55:26 +0900

    곽태윤_1214_01 즉시판매 페이지 구현중

--- wasRepo/team4_shoekream/src/main/java/com/shoekream/bidding/controller/SellNowControlloer.java
+++ wasRepo/team4_shoekream/src/main/java/com/shoekream/bidding/controller/SellNowControlloer.java
@@ -1,12 +1,16 @@
 package com.shoekream.bidding.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.shoekream.bidding.service.BiddingService;
+import com.shoekream.biddingVo.BiddingVo;
 
 @WebServlet("/sell/now")
 public class SellNowControlloer extends HttpServlet{
@@ -16,6 +20,44 @@ public class SellNowControlloer extends HttpServlet{
 		
 		req.getRequestDispatcher("/WEB-INF/views/sell/now.jsp").forward(req, resp);
 		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			BiddingVo BuyProductInfo = (BiddingVo)req.getSession().getAttribute("BuyProductInfo");
+				System.out.println("-----------------즉시판매-----------------");
+//				System.out.println("BuyNowController 에러확인 BuyProductInfo : " + BuyProductInfo);
+			
+			String productsNo = req.getParameter("productsNo");
+			String price = req.getParameter("price");
+			String size = req.getParameter("size");
+				System.out.println("BuyNowController 에러확인 productsNo : " + productsNo);
+				System.out.println("BuyNowController 에러확인 price : " + price);
+				System.out.println("BuyNowController 에러확인 size : " + size);
+			
+			BiddingVo vo = new BiddingVo();
+			vo.setProductsNo(productsNo);
+			vo.setPrice(price);
+			vo.setShoesSizes(size);
+			
+			BiddingService bs = new BiddingService();
+			Map<String, Object> m = bs.sellProductList(vo);
+			if (m == null) {
+				throw new Exception("예외 발생 : voList == null");
+			}
+			BiddingVo buyPrVo = (BiddingVo)m.get("buyPrVo");
+			BiddingVo sellPrVo = (BiddingVo)m.get("sellPrVo");
+			
+			
+			req.setAttribute("productsNo", productsNo);
+			req.setAttribute("buyPrVo", buyPrVo);
+			req.setAttribute("sellPrVo", sellPrVo);
+			req.getRequestDispatcher("/WEB-INF/views/sell/now.jsp").forward(req, resp);
+		} catch (Exception e) {
+			System.out.println("buyNow 중 예외 발생");
+			e.printStackTrace();
+		}
 	}
 	
 }
