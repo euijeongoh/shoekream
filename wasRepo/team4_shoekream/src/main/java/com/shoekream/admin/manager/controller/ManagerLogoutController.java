97a731ca42a1bea60a53f1a671db3a4335fb7c5f
orlando <glee1470@naver.com>
2023-12-14 08:44:30 +0900

    Revert "관리자 로그인 메소드 수정"
    This reverts commit fa24ef041c4237aab43dc86fa1a7a9edc3bdbcad.

--- wasRepo/team4_shoekream/src/main/java/com/shoekream/admin/manager/controller/ManagerLogoutController.java
+++ wasRepo/team4_shoekream/src/main/java/com/shoekream/admin/manager/controller/ManagerLogoutController.java
@@ -14,7 +14,7 @@ public class ManagerLogoutController extends HttpServlet{
 
 	// 로그아웃 처리
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getSession().invalidate();
 		
 		req.getSession().setAttribute("alertMsg", "로그아웃 완료");
