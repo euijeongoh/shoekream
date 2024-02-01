fae6a1c9e6da801999616fe8dd5836fe38b52268
orlando <glee1470@naver.com>
2024-02-01 16:40:06 +0900

    라이브러리 변경

--- wasRepo/team4_shoekream/src/main/java/com/shoekream/member/controller/EmailSendController.java
+++ wasRepo/team4_shoekream/src/main/java/com/shoekream/member/controller/EmailSendController.java
@@ -19,7 +19,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 @WebServlet("/send/email")
